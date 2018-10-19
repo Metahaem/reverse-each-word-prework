b6987bd60dcf04911d5c6ba218f9901869e49561
Tom Rowson <tom.rowson@gmail.com>
2018-10-19 14:28:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,3 @@ def reverse_each_word(sentence)
   sen_arr.join(" ")
 end
 
-def reverse_each_word(sentence)
-  sen_arr = sentence.split
-  sen_arr.collect { |word| word.reverse! }
-  sen_arr.join(" ")
-end
