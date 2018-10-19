84af75baabfbabcca787a02d5ce7418fa79083b6
Tom Rowson <tom.rowson@gmail.com>
2018-10-19 14:24:36 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
-  sen_arr = sentence.to_a
-  sentence.each { |word| word.reverse }
+  sen_arr = sentence.split
+  sen_arr.each { |word| word.reverse }
+  sen_arr.join(" ")
 end
   
